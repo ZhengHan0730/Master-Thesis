f9cfce460556d2dbf3804b932255e4598145e961
Zheng Han <zhan04279@gmail.com>
2025-03-18 13:59:32 +0100

    add data quality evaluation front page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import './Header.css'; // 引入CSS文件
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css"; // 引入 CSS 文件
+
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
         <img src="/uzh.png" alt="Universität Zürich Logo" />
-        {/* <span>Universität Zürich UZH</span> */}
+        
       </div>
-      <div className="title">
-        PREDANO Privacy Enhancement Toolbox
-      </div>
+
+      <div className="title">PREDANO Privacy Enhancement Toolbox</div>
+
       <nav className="nav">
-        <a href="/home" className="nav-link">Home</a>
-        <a href="/demo" className="nav-link">Demo</a>
-        <a href="/application" className="nav-link">Application</a>
-        <a href="/contact" className="nav-link">Contact</a>
+        <Link to="/home" className="nav-link">Home</Link>
+        <Link to="/demo" className="nav-link">Demo</Link>
+        <Link to="/application" className="nav-link">Application</Link>
+        <Link to="/evaluate" className="nav-link highlight">Evaluation</Link> {/* 新增导航 */}
+        <Link to="/contact" className="nav-link">Contact</Link>
       </nav>
-      {/* <div className="actions">
-        <button className="sign-in">Sign in</button>
-        <button className="register">Register</button>
-      </div> */}
     </header>
   );
 };
